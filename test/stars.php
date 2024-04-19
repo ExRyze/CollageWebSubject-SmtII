f858d3acabc37ccf2e2ba0b33135b0d384ef890c
ExRyze <vaisyagovinandas@gmail.com>
2024-04-19 08:19:28 +0800

    Latihan

--- test/stars.php
+++ test/stars.php
@@ -1,30 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <?php
-
-  // Latihan
-  echo "<table border=1>";
-  for ($i=1; $i <= 3; $i++) { 
-    echo "<tr>";
-    for ($j=1; $j <= 3; $j++) { 
-      echo "<td>$i,$j</td>";
-    }
-    echo "</tr>";
-  }
-  echo "</table>";
-  echo "<br>";
-
-  ?>
-</body>
-</html>
-
 <?php
 
 // Stars
