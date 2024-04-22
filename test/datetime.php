16faa68fed26dc5c32234287117dfeac534d66c5
ExRyze <vaisyagovinandas@gmail.com>
2024-04-22 13:47:38 +0800

    Minor

--- test/datetime.php
+++ test/datetime.php
@@ -4,11 +4,19 @@ Silahkan dicoba untuk built in fuction
 2. ⁠time
 3. ⁠mktime
 4. ⁠strtotime -->
+<?php
+echo 'Default Timezone: ' . date('d-m-Y H:i:s') . '</br>';
+date_default_timezone_set('Asia/Jakarta');
+echo 'Indonesian Timezone: ' . date('d-m-Y H:i:s')."<br>"."<br>";
+?>
 
 <?php 
 $date = date("d m Y");
 $time = time();
 $mk = mktime(0,0,0,12,31,2001);
-$str = strtotime("next Monday");
-var_dump($date, date("H:i:s", $time), date("d m Y H:i:s", $mk), date("d m Y H:i:s", $str));
+$str = strtotime("now");
+echo $time."<br>";
+echo (date("H:i:s", $time))."<br>";
+echo (date("d m Y H:i:s", $mk))."<br>";
+echo (date("d m Y H:i:s", $str))."<br>";
 ?>
