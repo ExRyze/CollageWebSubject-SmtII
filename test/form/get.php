eff2d3501fa920f77cf530b7118358c29f7bf02f
ExRyze <vaisyagovinandas@gmail.com>
2024-05-01 14:20:05 +0800

    Lat Form

---
+++ test/form/get.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form Get</title>
+</head>
+<body>
+  <form action="_get.php" method="get">
+    <input type="text" name="search" placeholder="Search">
+    <input type="submit" value="Cari">
+  </form>
+</body>
+</html>
